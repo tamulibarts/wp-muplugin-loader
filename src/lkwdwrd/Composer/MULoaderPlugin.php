0776a6639e1e65e471b81c76b7f72b314b297730
Joseph Rafferty <joraff@gmail.com>
2019-09-04 11:38:01 -0500

    Avoids extrapolating a DIR reference in mu-loader

--- src/lkwdwrd/Composer/MULoaderPlugin.php
+++ src/lkwdwrd/Composer/MULoaderPlugin.php
@@ -144,7 +144,7 @@ class MULoaderPlugin implements PluginInterface, EventSubscriberInterface {
 		}
 		file_put_contents(
 			$muPath . 'mu-require.php',
-			"<?php\nrequire_once __DIR__ . '${toLoader}';\n"
+			'<?php' . PHP_EOL . 'require_once __DIR__ . \'' . $toLoader . '\';' . PHP_EOL
 		);
 	}
 	/**
